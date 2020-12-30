1567c25f70452db128f68711baa94ec76520d1fc
kleancantina <mack-jonas@web.de>
2020-12-30 08:54:03 +0100

    Make pytest work to run all test scripts

--- scripts/run_Upwind_with_PINN/test_Upwind_with_PINN.py
+++ scripts/run_Upwind_with_PINN/test_Upwind_with_PINN.py
@@ -5,7 +5,7 @@ import numpy as np
 
 pinn = PINN()
 pinn.generate_training_data(n_initial=50, n_boundary=25, equidistant=False)
-pinn.perform_training(max_n_epochs=1000, min_mse=0.05, track_losses=True, batch_size='full')
+pinn.perform_training(max_n_epochs=3, min_mse=0.05, track_losses=True, batch_size='full')
 
 # Plot solution
 print(f"PINN error: {pinn.mse}")
@@ -33,6 +33,7 @@ print(f"Upwind MSE: {upwind.get_mean_squared_error()}")
 # generate_contour_and_snapshots_plot(u=upwind.u, savefig_path='scripts/run_Upwind_with_PINN/Upwind_solution.jpg')
 
 # ToDo: Change file name
-file_name = "epochs=1000.jpg"
-generate_two_contour_and_snapshots_plots(u1=pinn.u_pred, u2=upwind.u_numerical,
-                                         savefig_path=f'scripts/run_Upwind_with_PINN/preliminary_results/{file_name}')
+generate_two_contour_and_snapshots_plots(u1=pinn.u_pred, u2=upwind.u_numerical)
+# file_name = "epochs=1000.jpg"
+# generate_two_contour_and_snapshots_plots(u1=pinn.u_pred, u2=upwind.u_numerical,
+#                                          savefig_path=f'scripts/run_Upwind_with_PINN/preliminary_results/{file_name}')
